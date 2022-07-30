c2dbfe8dabd5a786451f468f56256d99fd304136
Justin Riley Uy <justin_riley_uy@dlsu.edu.ph>
2022-07-30 12:53:48 +0800

    restarted

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    loader: "akamai",
-    path: "/",
+  swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path: '/',
   },
-  basePath:"/rileyuy.github.io",
-  assetPrefix:"/rileyuy.github.io"
 }
 
 module.exports = nextConfig
