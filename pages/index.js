7c1abe554fdcde761199d35a4d5b0dd2490e0300
Justin Riley Uy <justin_riley_uy@dlsu.edu.ph>
2022-08-10 03:11:34 +0800

    Added mobile/tablet responsiveness and added about me section

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,17 @@ import { Layout } from "../components/Layout/Layout";
 import { FaGithubSquare, FaFacebookSquare, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
+  const resumeURL = "./public/downloadables/Resume.pdf";
+  const resumeName = "Resume.pdf";
+
   return (
     <Layout>
       <Head>
         <title>{"Home"}</title>
         <link rel="icon" href="/r-alt.png" />
       </Head>
-      <section className="w-full h-screen lg:h-screen justify-between relative flex flex-col lg:flex-row bg-dark px-16 md:px-32 py-16 items-center">
-        <div className="lg:w-1/2 w-full h-full flex flex-col justify-center mx-auto lg:mr-24 lg:mt-0 my-12">
+      <section className="w-full h-fit lg:h-screen justify-between relative flex flex-col lg:flex-row bg-dark px-8 md:px-32 py-16 items-center">
+        <div className="lg:w-1/2 w-full h-full flex flex-col justify-center mx-auto lg:mr-24 lg:mt-0 my-4 md:my-8 lg:my-12 px-12 lg:px-0">
           <div className="rounded-full overflow-hidden drop-shadow-xl bg-white">
             <Image
               layout="responsive"
@@ -23,40 +26,97 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="lg:1-/2 h-full flex flex-col justify-center mx-auto text-center lg:text-left">
+        <div className="h-fit lg:h-full w-full flex flex-col justify-center mx-auto text-center lg:text-left">
           {/* <p className="font-lato font-bold text-4xl text-home_blue">Hello, I'm Riley.</p>
-          <p className="font-lato text-4xl text-white">
+          <p className="font-lato text-4xl ">
             I'm a 3rd year student taking Bachelor of Science in Computer
             Science in De La Salle University.
           </p>
-          <p className="font-lato text-4xl text-white">Aspiring developer and UX designer.</p> */}
-          <div className="pb-8">
-            <p className="font-lato font-bold text-4xl text-white">
-              <span className="text-home_blue">Hello, I&quot;m Riley. </span>
-              
-              <br/><br/>I&quot;m a 3rd year student taking Bachelor of
-              Science in Computer Science in De La Salle University. 
-              
-              <br/><br/>Aspiring developer and UX designer.
+          <p className="font-lato text-4xl ">Aspiring developer and UX designer.</p> */}
+          <div className="mb-8">
+            <p className="font-lato  text-xl md:text-4xl ">
+              <span className="text-home_blue font-bold">
+                Hello, I&rsquo;m Riley.{" "}
+              </span>
+              <br />
+              <br />
+              I&rsquo;m a 3rd year student taking Bachelor of Science in
+              Computer Science in De La Salle University.
+              <br />
+              <br />
+              Aspiring developer and UX designer.
             </p>
           </div>
-          <div className="flex flex-col md:flex-row items-center h-24 lg:h-12 w-full lg:justify-start justify-center">
-            <div className="flex flex-row lg:mr-8">
+          <div className="flex flex-col md:flex-row items-center h-full md:h-12 w-full lg:justify-start justify-center">
+            <div className="flex flex-row lg:mr-8 justify-between lg:justify-start lg:w-fit w-full lg:mx-0  md:mb-0 mb-8 lg:px-0 px-16">
               <a href="https://www.linkedin.com/in/riley-uy/">
-                <FaLinkedin className="w-14 h-full cursor-pointer text-button_dark mr-4" />
+                <FaLinkedin className="w-14 h-full cursor-pointer text-button_dark lg:mr-4" />
               </a>
 
               <a href="https://github.com/rileyuy">
-                <FaGithubSquare className="w-14 h-full cursor-pointer text-button_dark mr-4" />
+                <FaGithubSquare className="w-14 h-full cursor-pointer text-button_dark lg:mr-4" />
               </a>
 
               <a href="https://www.facebook.com/rileyuyy/">
-                <FaFacebookSquare className="w-14 h-full cursor-pointer text-button_dark mr-4" />
+                <FaFacebookSquare className="w-14 h-full cursor-pointer text-button_dark lg:mr-4" />
               </a>
             </div>
-            <button className="bg-button_dark rounded-lg h-full px-6 font-lato text-white text-3xl font-bold">
-              Download CV
-            </button>
+
+            <a
+              className=" h-full flex items-center justify-center"
+              target="_blank"
+              href="https://drive.google.com/file/d/1Zxd7VdXUWcLHdPOxO7o01ofCCj1cqBe4/view?usp=sharing"
+            >
+              <button className="bg-button_dark text-white rounded-lg h-14 lg:h-full px-6 font-lato  text-3xl font-bold">
+                Download CV
+              </button>
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className="w-full h-fit justify-center relative flex flex-col items-center bg-dark py-16 px-8 md:px-32">
+        <div className=" w-full h-full flex justify-center flex-col items-center lg:pb-6 pb-12">
+          <h1 className="text-4xl lg:text-6xl font-extrabold  font-lato pb-4 lg:pb-12">about me</h1>
+          <p className="text-xl lg:text-3xl font-lato text-center">
+            Hello, my name is Riley. I&rsquo;m a 3rd year student taking my
+            degree in Computer Science in De La Salle University-Manila. Ever
+            since high school I&rsquo;ve been intrigued about the impact of
+            programming and how powerful it can be for the future. That same
+            year where I was introduced to programming, I also learned about UX
+            design; I learned how design can be as impactful and powerful as the
+            functions and features of an application.
+            <br />
+            <br />I have a long way to go because there&rsquo;s still so much I
+            haven&rsquo;t experienced first hand in the field of development and
+            design that I have yet to learn.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-between w-full">
+          <div className="flex text-center flex-col md:text-left md:mb-0 mb-8">
+            <h1 className="text-2xl md:text-4xl font-bold  font-lato pb-2">
+              education
+            </h1>
+            <p className="text-xl font-lato ">
+              De La Salle University - Manila (19-23) 🟢
+              <br />
+              Jubilee Christian Academy (06-19) 🔺
+            </p>
+          </div>
+          <div className="flex text-center  flex-col md:text-right">
+            <h1 className="text-2xl md:text-4xl font-bold font-lato pb-2">
+              hobbies & interests
+            </h1>
+            <p className="text-xl font-lato ">
+              🖊️ UX Design
+              <br />
+              🌐 Web development 
+              <br />
+              🎮 Gaming 
+              <br />
+              🏸 Badminton 
+              <br />
+              🎥 Video editing 
+            </p>
           </div>
         </div>
       </section>
